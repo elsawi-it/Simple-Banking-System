a3a03e511e42c1fc9ffead0b55b3f0c2c0ae4ab8
Ahmed <elsawi.it@gmail.com>
2020-09-28 00:59:10 +0300

    Update Main.java

--- src/main/java/banking/Main.java
+++ src/main/java/banking/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SqliteDB.sqliteDBConnection();
         BankScreens.mainScreen();
-        SqliteDB.closeSqliteDBConnection();
-
 
     }
 
